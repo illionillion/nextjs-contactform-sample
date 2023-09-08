a224ae1ea17b392e422400f886c32a6e4efa6ded
yuto ichinose <yuto.0526.sano@gmail.com>
2023-09-09 07:48:13 +0900

    feat: データベースに登録する処理追加

--- app/pages/api/send.ts
+++ app/pages/api/send.ts
@@ -1,20 +1,31 @@
+import mysql_connection from "@/lib/db/connection";
 import type { NextApiRequest, NextApiResponse } from "next";
 
-type ResponseData = {
-  message: string;
-};
-
-type RequestData = {
-  name: string;
-  email: string;
-  emailcontent_question: string;
-};
-
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<ResponseData>
+  res: NextApiResponse
 ) {
-  console.log(req.body);
+  if (req.method === "POST") {
+    try {
+      const connection = await mysql_connection();
 
-  res.status(200).json({ message: "OK!!" });
+      const query = `INSERT INTO contact_table (name, email, content_question) VALUES (?, ?, ?)`;
+      await connection.execute(query, [
+        req.body?.name,
+        req.body?.email,
+        req.body?.content_question,
+      ]);
+
+      res.status(200).json({ message: "送信に成功しました。" });
+    } catch (error) {
+      res.status(500).json({ message: "送信に失敗しました。", error: error });
+    }
+  } else {
+    res
+      .status(405)
+      .json({
+        message: "送信に失敗しました。",
+        error: "POSTメソッドのみ許可されます。",
+      });
+  }
 }
