e1390ad1dfbecf1bb4c52b4010e73b3f43651012
yuto ichinose <yuto.0526.sano@gmail.com>
2023-09-09 15:21:01 +0900

    update: envから読み込み

--- app/lib/db/connection.ts
+++ app/lib/db/connection.ts
@@ -4,10 +4,10 @@ import mysql from "mysql2/promise";
 
 const mysql_connection = async () =>
   await mysql.createConnection({
-    host: "mysql",
-    user: "root",
-    password: "password",
-    database: "contactform",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
   });
 
 export default mysql_connection;
